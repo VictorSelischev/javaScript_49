271058e0c4de40b84fea81af096afe1c4e8d99d3
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-06-06 22:34:42 +0300

    Работа цикла for of для массивов

---
+++ js/module-02.js
@@ -0,0 +1,21 @@
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+
+
+
+
+const clients = ["Mango", "Ajax", "Poly"];
+
+for (const client of clients) {
+  console.log(client);
+}
+
+const string = "javascript";
+
+for (const character of string) {
+  console.log(character);
+}
