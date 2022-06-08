4944b9eab9d16319dd41bfcd4ad464d683fbced2
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-06-08 18:23:26 +0300

    Update module-02.js

--- js/module-02.js
+++ js/module-02.js
@@ -20,32 +20,57 @@
 //   console.log(character);
 // }
 
-const woods = ['beech', 'rowan', 'apple'];
 
-for (let i = 0; i < woods.length; i += 1) {
-  console.log(woods[i]);
-}
 
-const newWood = woods.length;
-console.log(woods.length);
 
-console.log('--------------------------');
 
-const newWood2 = woods.unshift('oak');
+// const woods = ['beech', 'rowan', 'apple'];
 
-for (let i = 0; i < woods.length; i += 1) {
-  console.log(woods[i]);
-}
+// for (let i = 0; i < woods.length; i += 1) {
+//   console.log(woods[i]);
+// }
 
-console.log(newWood2);
+// const newWood = woods.length;
+// console.log(woods.length);
 
-console.log('--------------------------');
+// console.log('--------------------------');
 
-const newWood3 = woods.push('cherry');
+// const newWood2 = woods.unshift('oak');
 
-for (let i = 0; i < woods.length; i += 1) {
-  console.log(woods[i]);
-}
+// for (let i = 0; i < woods.length; i += 1) {
+//   console.log(woods[i]);
+// }
 
-console.log(newWood3);
+// console.log(newWood2);
 
+// console.log('--------------------------');
+
+// const newWood3 = woods.push('cherry');
+
+// for (let i = 0; i < woods.length; i += 1) {
+//   console.log(woods[i]);
+// }
+
+// console.log(newWood3);
+
+
+
+// Функциональное выражение (function expression)
+// const multiply = function (x, y, z) {
+//   console.log(`Результат умножения равен ${x * y * z}`);
+// };
+
+// console.log(multiply(2, 3, 5));
+// console.log(multiply(3, 7, 8));
+
+
+
+const woods = ['beech', 'rowan', 'apple', 'oak', 'cherry'];
+console.log(woods);
+console.table(woods);
+
+const woodsLast = woods.length - 1;
+console.log('Индекс последнего элемента: ', woodsLast);
+
+woods[woodsLast] = 'birch';
+console.table(woods);
