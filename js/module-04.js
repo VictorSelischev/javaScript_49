eefd7316b67bee24931db507d224a8c162c7d1b8
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-06-23 21:43:10 +0300

    Разбирает работу метода reduce

--- js/module-04.js
+++ js/module-04.js
@@ -1,14 +1,75 @@
-// Декларативный подход
-const numbers = [1, 2, 3, 4, 5];
-const filteredNumbers = numbers.filter(value => value > 3);
-console.log(filteredNumbers); // [4, 5]
+// // Декларативный подход
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = numbers.filter(value => value > 3);
+// console.log(filteredNumbers); // [4, 5]
 
 
 
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-const result = words.filter(word => word.length > 6);
+// const result = words.filter(word => word.length > 6);
 
-console.log(result);
-// expected output: Array ["exuberant", "destruction", "present"]
+// console.log(result);
+// // expected output: Array ["exuberant", "destruction", "present"]
+
+
+
+
+
+
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+
+// Пройдем по всем элементам коллекции и прибавим значения свойства likes
+// к аккумулятору, начальное значение которого укажем 0.
+// const likes = tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
+
+// console.log(likes); // 32
+
+// Наверное подсчет лайков не одиночная операция, поэтому напишем функцию
+// для подсчета лайков из коллекции
+const countLikes = tweets => {
+  return tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
+};
+
+console.log(`Лайков: `, countLikes(tweets)); // 32
+
+const getTags = tweets =>
+  tweets.reduce((allTags, tweet) => {
+    allTags.push(...tweet.tags);
+
+    return allTags;
+  }, []);
+
+const tags = getTags(tweets);
+
+console.log(tags);
+
+// Вынесем callback-функцию отдельно, а в reducе передадим ссылку на нее.
+// Это стандартная практика если callback-функция довольно большая.
+
+// Если в объекте-аккумуляторе acc нету своего свойства с ключем tag,
+// то создаем его и записывает ему значение 0.
+// В противном случае увеличиваем значение на 1.
+const getTagStats = (acc, tag) => {
+  if (!acc.hasOwnProperty(tag)) {
+    acc[tag] = 0;
+  }
+
+  acc[tag] += 1;
+
+  return acc;
+};
+
+// Начальное значение аккумулятора это пустой объект {}
+const countTags = tags => tags.reduce(getTagStats, {});
+
+const tagCount = countTags(tags);
+console.log(tagCount);
