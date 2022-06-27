d354d1521b619cca592acec8c7048f0f84f5a7b7
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-06-27 21:00:33 +0300

    Решает задачи до 24 включительно

---
+++ js/avtoproverka-04.js
@@ -0,0 +1,181 @@
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, onSuccess, onError) {
+    
+//     for (const pizza of this.pizzas) {
+//       if (pizzaName === pizza) {
+//         return onSuccess(pizzaName);
+//       }
+
+//     }
+
+//     return onError (`There is no pizza with a name ${pizzaName} in the assortment.`);
+    
+//   },
+// };
+// // Change code above this line
+
+// console.log(pizzaPalace);
+// console.table(pizzaPalace);
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+
+// // Method calls with callbacks
+// console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+
+
+
+
+
+
+
+
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] % 2 === 0) {
+//   //     numbers[i] = numbers[i] + value;
+//   //   }
+//   // }
+
+//   const newArray = [];
+
+//   numbers.forEach(element => {
+//     if (element % 2 === 0) {
+//       element += value
+//     }
+
+//     newArray.push(element);
+//   });
+
+//   return newArray;
+
+//   // Change code above this line
+// }
+
+
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+// console.log(changeEven([17, 24, 68, 31, 42], 100));
+// console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+
+
+
+
+const users =
+    [
+        {
+            name: "Moore Hensley",
+            email: "moorehensley@indexia.com",
+            eyeColor: "blue",
+            friends: ["Sharron Pace"],
+            isActive: false,
+            balance: 2811,
+            gender: "male",
+            age: 37
+        },
+        {
+            name: "Sharlene Bush",
+            email: "sharlenebush@tubesys.com",
+            eyeColor: "blue",
+            friends: ["Briana Decker", "Sharron Pace"],
+            isActive: true,
+            balance: 3821,
+            gender: "female",
+            age: 34
+        },
+        {
+            name: "Ross Vazquez",
+            email: "rossvazquez@xinware.com",
+            eyeColor: "green",
+            friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+            isActive: false,
+            balance: 3793,
+            gender: "male",
+            age: 24
+        },
+        {
+            name: "Elma Head",
+            email: "elmahead@omatom.com",
+            eyeColor: "green",
+            friends: ["Goldie Gentry", "Aisha Tran"],
+            isActive: true,
+            balance: 2278,
+            gender: "female",
+            age: 21
+        },
+        {
+            name: "Carey Barr",
+            email: "careybarr@nurali.com",
+            eyeColor: "blue",
+            friends: ["Jordan Sampson", "Eddie Strong"],
+            isActive: true,
+            balance: 3951,
+            gender: "male",
+            age: 27
+        },
+        {
+            name: "Blackburn Dotson",
+            email: "blackburndotson@furnigeer.com",
+            eyeColor: "brown",
+            friends: ["Jacklyn Lucas", "Linda Chapman"],
+            isActive: false,
+            balance: 1498,
+            gender: "male",
+            age: 38
+        },
+        {
+            name: "Sheree Anthony",
+            email: "shereeanthony@kog.com",
+            eyeColor: "brown",
+            friends: ["Goldie Gentry", "Briana Decker"],
+            isActive: true,
+            balance: 2764,
+            gender: "female",
+            age: 39
+        }
+    ];
+
+    // Change code below this line
+const getUsersWithFriend = (users, friendName) => {
+   const allFriends = users.filter(user => {
+    
+    for (const friend of user.friends) {
+        if (friend === friendName) return true;
+    }
+    
+    });
+
+    return allFriends;
+};
+// Change code above this line
+
+
+console.log(getUsersWithFriend(users, "Briana Decker"));
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
+console.log(getUsersWithFriend(users, "Adrian Cross"));
+
+// Привіт! Я flatMap не використовував. Використовував filter, в ньому деструктуризував friends і через includes шукав friendName, це все.
+
+// А у меня получилось через filter и внутри использовал for...of (пытался через forEach, но как-то громоздко получалось и ничего не работало)
+
+
+
+
+
+
+
+
