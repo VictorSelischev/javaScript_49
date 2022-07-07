4af030f466d8f19bf4f0ade7c5197f7dec4b6042
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-07 18:53:53 +0300

    Update avtoproverka-05.js

--- js/avtoproverka-05.js
+++ js/avtoproverka-05.js
@@ -1,38 +1,76 @@
-const historyService = {
-  orders: [
-    { email: "jacob@hotmail.com", dish: "Burrito" },
-    { email: "solomon@topmail.net", dish: "Burger" },
-    { email: "artemis@coldmail.net", dish: "Pizza" },
-    { email: "solomon@topmail.net", dish: "Apple pie" },
-    { email: "jacob@hotmail.com", dish: "Taco" },
-  ],
-  // Change code below this line
-  getOrdersLog() {
-    const ordersLogs = this.orders.map(order => `email: ${order.email} dish: ${order.dish}`);
-    // console.log(ordersLogs);
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     const ordersLogs = this.orders.map(order => `email: ${order.email} dish: ${order.dish}`);
+//     // console.log(ordersLogs);
       
-    const stringLog = ordersLogs.join(" - ");
-    return stringLog;
-  },
-  getEmails() {
-    const emails = this.orders.map(order => order.email);
-    const uniqueEmails = new Set(emails);
-    return [...uniqueEmails];
-  },
-  getOrdersByEmail(email) {
-    return this.orders.filter(order => order.email === email);
-  },
+//     const stringLog = ordersLogs.join(" - ");
+//     return stringLog;
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
+
+
+
+// console.log(historyService.getOrdersLog());
+// console.table(historyService.getEmails());
+// console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
+// console.log(historyService.getOrdersByEmail("artemis@coldmail.net"));
+
+
+
+
+
+class Car {
+  // Change code below this line
+  
+  static MAX_PRICE = 50000;
+
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+
+    if (newPrice > Car.MAX_PRICE) {
+      return;
+    }
+    
+      this.#price = newPrice;
+    
+  }
   // Change code above this line
-};
+}
 
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
 
+audi.price = 49000;
+console.log(audi.price); // 49000
 
-console.log(historyService.getOrdersLog());
-console.table(historyService.getEmails());
-console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
-console.log(historyService.getOrdersByEmail("artemis@coldmail.net"));
+audi.price = 51000;
+console.log(audi.price); // 49000
 
-
-
-
+// console.log(Car);
 
