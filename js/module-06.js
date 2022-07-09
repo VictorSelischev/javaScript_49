5a7ee87f8ae181ecceed2564c1afa80629eed333
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-09 21:57:11 +0300

    Модуль 6. Конспект

---
+++ js/module-06.js
@@ -0,0 +1,34 @@
+// // console.log(document);
+
+// const body = document.body;
+// // console.log(body);
+
+// const list = body.firstElementChild;
+// console.log(list);
+
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
+
+// const listItems = list.children;
+// console.log(listItems);
+
+
+
+
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '18px';
+console.log(listWithId);
+
+const listWithClass = document.querySelector('.menu');
+console.log(listWithClass);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = 'blue';
+console.log(firstMenuItem);
