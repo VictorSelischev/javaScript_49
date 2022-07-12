c40a1c8610889bbae555b9291c2a0a5f67ba9608
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-12 12:27:52 +0300

    Модуль 6 - DOM

--- js/module-06.js
+++ js/module-06.js
@@ -15,20 +15,87 @@
 
 
 
-const listWithId = document.querySelector('#menu');
-listWithId.style.textTransform = 'uppercase';
-listWithId.style.fontSize = '18px';
-console.log(listWithId);
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '28px';
+// console.log(listWithId);
 
-const listWithClass = document.querySelector('.menu');
-console.log(listWithClass);
+// const listWithClass = document.querySelector('.menu');
+// console.log(listWithClass);
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName);
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
 
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass);
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
 
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = 'blue';
-console.log(firstMenuItem);
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = 'blue';
+// console.log(firstMenuItem);
+
+
+
+
+// //===================================================
+// const message = document.querySelector("#message");
+// console.log(message.value); // Default textarea message
+
+// //===================================================
+// const activeLink = document.querySelector(".btn.active");
+// console.log(activeLink.href); // https://s.codepen.io/about
+
+// const noActiveLink = document.querySelector('.btn');
+// console.log(noActiveLink.href);
+
+// //===================================================
+// const image = document.querySelector(".image");
+// console.log(image.src); // https://placeimg.com/640/480/animals
+// image.src = 'https://placeimg.com/640/480/tech';
+
+
+// const text = document.querySelector('.btn.gallery');
+// console.log(text.textContent);
+
+// text.textContent = 'Галлерея (по-русски)';
+// console.log(text.textContent);
+
+// text.style.backgroundColor = '#1E90FF';
+// text.style.color = 'white';
+
+// const imgHtml = document.querySelector('.image');
+// console.log(imgHtml);
+// imgHtml.setAttribute('alt', 'Каждый раз новая картинка')
+// console.log(imgHtml.alt);
+// console.log(imgHtml.attributes);
+
+// //===================================================
+
+
+
+
+const article = document.querySelector(".article");
+console.log(article.innerHTML);
+
+const title = document.querySelector(".article .title");
+console.log(title.innerHTML);
+
+const text = document.querySelector(".article .text");
+console.log(text.innerHTML);
+
+const link = document.querySelector(".article .link");
+console.log(link.innerHTML);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
