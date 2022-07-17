a2fb1a14f2315ea9a4ef208cf00ec969c95cce5b
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-17 22:19:33 +0300

    Подключает библиотеку Lodash

--- js/lesson-13.js
+++ js/lesson-13.js
@@ -1,3 +1,29 @@
-const divListener = document.querySelector('[data-name="css"]');
-console.log(divListener.dataset);
-console.log(divListener.dataset.value);
+// const divListener = document.querySelector('[data-name="css"]');
+// console.log(divListener.dataset);
+// console.log(divListener.dataset.value);
+
+// console.log(_.ceil(6040, -2));
+// const result = _.add(2, 3);
+// console.log(result);
+
+
+
+const vanillaOutput = document.querySelector(".output.vanilla");
+const throttledOutput = document.querySelector(".output.throttled");
+const eventCounter = {
+  vanilla: 0,
+  throttled: 0
+};
+
+document.addEventListener("scroll", () => {
+  eventCounter.vanilla += 1;
+  vanillaOutput.textContent = eventCounter.vanilla;
+});
+
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    eventCounter.throttled += 1;
+    throttledOutput.textContent = eventCounter.throttled;
+  }, 300)
+);
