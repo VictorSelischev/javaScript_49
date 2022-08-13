5eb22b8889248a95187ffe649174e6103f6948da
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-13 16:46:48 +0300

    9-й модуль

--- js/module-10.js
+++ js/module-10.js
@@ -1,32 +1,13 @@
-// Change value of isSuccess variable to call resolve or reject
-const isSuccess = true;
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
-    } else {
-      reject("Error! Error passed to reject function");
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
-  }, 2000);
-});
-
-// Will run first
-console.log("Before promise.then()");
-
-// Registering promise callbacks
-promise.then(
-  // onResolve will run third or not at all
-  value => {
-    console.log("onResolve call inside promise.then()");
-    console.log(value); // "Success! Value passed to resolve function"
-  },
-  // onReject will run third or not at all
-  error => {
-    console.log("onReject call inside promise.then()");
-    console.log(error); // "Error! Error passed to reject function"
-  }
-);
-
-// Will run second
-console.log("After promise.then()");
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
