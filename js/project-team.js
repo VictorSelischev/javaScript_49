1b6728b2df7642a6514111dac468ae50c07fb507
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-27 17:29:16 +0300

    Update project-team.js

--- js/project-team.js
+++ js/project-team.js
@@ -1,13 +1,20 @@
-// import { saveLocalStorage, loadLocalStorage, removeLocalStorage } from "./localStorage";
-
-// import getMoviesApi from './getMoviesApi';
-// import { appendMoviesMarkup } from './moviesMarkup';
+import { saveLocalStorage, loadLocalStorage, removeLocalStorage } from "./localStorage";
+import { appendMoviesMarkup } from './moviesMarkup';
 
 
+const watchedBtn = document.querySelector('button[data-action="watched"]');
+const queueBtn = document.querySelector('button[data-action="queue"]');
 
 
-
+if (queueBtn.classList.contains('.js-current')) {
+  const queueKey = loadLocalStorage('queueKey');
+  appendMoviesMarkup(queueKey);
+  return;
+}
+watchedBtn.classList.contains('.js-current');
 const watchedKey = loadLocalStorage('watchedKey');
+
+
 console.log(watchedKey);
 
 if (watchedKey === null || watchedKey === undefined) {
@@ -43,18 +50,28 @@ function removeLocalStorage(key) {
   return localStorage.removeItem(key);
 }
 
+function onPageStart() {
+  getMovies.resetPage();
+  getMovies.getStartMovies().then(movies => {
+    appendMoviesMarkup(movies);
+  });
+}
 
-function appendMoviesMarkup() {
+function appendMoviesMarkup(movies) {
+  movies.forEach(movie => {
+    const movieTitle = movie.title;
+    const moviePoster = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const movieReleaseDate = movie.release_date.split('').slice(0, 4).join('');
+    const movieGenres = getGenres(movie.genre_ids);
 
-    const body = document.querySelector('body');
-
-    const movieMarkup = `<li class="film-list__item"><img src="" alt="" class="film-list__item-poster" loading="lazy">
-     <h3 class="film-list__item-title">Title</h3>
-     <div class="film-list__item-info"><span class="film-list__item-genres">:Жанры</span>|<span class="film-list__item-year">Год</span></div>
+    const movieMarkup = `<li class="film-list__item"><img src="${moviePoster}" alt="${movieTitle}" class="film-list__item-poster" loading="lazy">
+     <h3 class="film-list__item-title">${movieTitle}</h3>
+     <div class="film-list__item-info"><span class="film-list__item-genres">${movieGenres}</span>|<span class="film-list__item-year">${movieReleaseDate}</span></div>
    </li>`;
 
-    body.insertAdjacentHTML('beforeend', movieMarkup);
-  }
+    moviesGallery.insertAdjacentHTML('beforeend', movieMarkup);
+  });
+}
 
 
 function getGenres(ids) {
